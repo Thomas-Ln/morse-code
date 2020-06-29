1096a7052ceb4bd9c6415e33c8b2586162b8891f
Thomas <thomas-ln@laposte.net>
2020-06-29 17:42:37 +0200

    Updates

---
+++ precache-manifest.c0decac1c92e3a9746ac52d7f12dd96e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4fc664ecad3915b82b605c6779438e",
+    "url": "/morse-code/index.html"
+  },
+  {
+    "revision": "5a1e63dd3cd4ebe38fba",
+    "url": "/morse-code/static/css/2.4075f44a.chunk.css"
+  },
+  {
+    "revision": "a708b56747528cf26287",
+    "url": "/morse-code/static/css/main.4b31dfa9.chunk.css"
+  },
+  {
+    "revision": "5a1e63dd3cd4ebe38fba",
+    "url": "/morse-code/static/js/2.9cc18c23.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/morse-code/static/js/2.9cc18c23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a708b56747528cf26287",
+    "url": "/morse-code/static/js/main.ebf4e67c.chunk.js"
+  },
+  {
+    "revision": "a5df0040020b83f6e32a743708260f3e",
+    "url": "/morse-code/static/js/main.ebf4e67c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d653144d3a21ce672e3d",
+    "url": "/morse-code/static/js/runtime-main.9eb98829.js"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/morse-code/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/morse-code/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/morse-code/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/morse-code/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/morse-code/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/morse-code/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/morse-code/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/morse-code/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/morse-code/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/morse-code/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/morse-code/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/morse-code/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/morse-code/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/morse-code/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/morse-code/static/media/fa-solid-900.f3a7d3b5.woff"
+  }
+]);
